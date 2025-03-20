eb1b6df208087556e60736a4c295e39322d0431d
jackarnold84 <jackarnold84@gmail.com>
2025-03-20 08:41:38 -0500

    2025 config updates

--- service/lambdas/bracketKeyUpdate/config.py
+++ service/lambdas/bracketKeyUpdate/config.py
@@ -3,24 +3,24 @@ import datetime
 # set the date of each round
 game_date_to_wins = {
     # r64
-    datetime.date(2024, 3, 21): 0,
-    datetime.date(2024, 3, 22): 0,
+    datetime.date(2025, 3, 20): 0,
+    datetime.date(2025, 3, 21): 0,
 
     # r32
-    datetime.date(2024, 3, 23): 1,
-    datetime.date(2024, 3, 24): 1,
+    datetime.date(2025, 3, 22): 1,
+    datetime.date(2025, 3, 23): 1,
 
     # s16
-    datetime.date(2024, 3, 28): 2,
-    datetime.date(2024, 3, 29): 2,
+    datetime.date(2025, 3, 27): 2,
+    datetime.date(2025, 3, 28): 2,
 
     # e8
-    datetime.date(2024, 3, 30): 3,
-    datetime.date(2024, 3, 31): 3,
+    datetime.date(2025, 3, 29): 3,
+    datetime.date(2025, 3, 30): 3,
 
     # f4
-    datetime.date(2024, 4, 6): 4,
+    datetime.date(2025, 4, 5): 4,
 
     # ncg
-    datetime.date(2024, 4, 8): 5,
+    datetime.date(2025, 4, 7): 5,
 }
